37398e52ff7e558019a71f394f3fd9d92819c148
Ruben De Andrade <ruben.deandrade@fabernovel.com>
2023-05-14 16:19:56 +0200

    (fix): add logs to debug revalidate api route
    Signed-off-by: Ruben De Andrade <ruben.deandrade@fabernovel.com>

--- webapp/src/pages/api/revalidate.ts
+++ webapp/src/pages/api/revalidate.ts
@@ -4,15 +4,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const SANITY_WEBHOOK_SECRET = process.env.NEXT_PUBLIC_SANITY_WEBHOOK_SECRET ?? "";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (process.env.NODE_ENV !== "production") {
-    return;
-  }
-  const signature = req.headers[SIGNATURE_HEADER_NAME];
-  const isValid =
-    typeof signature === "string"
-      ? isValidSignature(JSON.stringify(req.body), signature, SANITY_WEBHOOK_SECRET)
-      : false;
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+  const isValid = isValidSignature(JSON.stringify(req.body), signature, SANITY_WEBHOOK_SECRET);
 
+  console.log(`body: ${JSON.stringify(req.body)}`);
+  console.log(`signature: ${signature}`);
+  console.log(`secret: ${SANITY_WEBHOOK_SECRET}`);
   console.log(`===== Is the webhook request valid? ${isValid}`);
 
   // Validate signature
@@ -22,12 +19,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
-    const pathToRevalidate = req.body.slug.current;
+    const contentType: string = req.body._type;
 
-    console.log(`===== Revalidating: ${pathToRevalidate}`);
+    console.log(`===== Revalidating: ${contentType}`);
 
-    await res.revalidate(pathToRevalidate);
-
+    if (contentType === "event") {
+      console.log(`===== Revalidating: timeline`);
+      await res.revalidate("/fr/timeline");
+    }
     return res.json({ revalidated: true });
   } catch (err) {
     // Could not revalidate. The stale page will continue to be shown until
