0ae6755c17ce9e4ed53b531ff5cabdf3d884434a
Ruben De Andrade <28705409+RubenDeAndrade@users.noreply.github.com>
2023-12-30 16:15:05 +0100

    (feature): Upcoming events section (#16)
    Signed-off-by: Ruben de Andrade <ruben.deandrade@fabernovel.com>

--- webapp/src/domains/home/upcoming-event-board/UpcomingEventBoard.tsx
+++ webapp/src/domains/home/upcoming-event-board/UpcomingEventBoard.tsx
@@ -3,32 +3,39 @@ import type { Event } from "@core/event/data/EventModel";
 import dayjs from "dayjs";
 import { Title } from "@core/common/components/title/Title";
 import { Text } from "@ui-kit/components/text/Text";
+import { useTranslation } from "@core/i18n/useTranslation";
+import { homeNamespaces } from "../homeNamespaces";
+import { ExternalLink } from "@ui-kit/components/external-link/ExternalLink";
 
 type UpcomingEventBoardProps = {
   events: Event[];
   className?: string;
 };
 
+const MAX_EVENTS_DISPLAYED = 5;
+
 export const UpcomingEventBoard = ({ events, className }: UpcomingEventBoardProps) => {
+  const { t } = useTranslation(homeNamespaces);
+
   return (
     <div className={className}>
-      <Title className="mb-16">Upcoming Events</Title>
+      <Title className="mb-16">{t("home:upcoming-event-section.title")}</Title>
       <ul className="mx-12 flex flex-col">
         {events.length === 0 ? (
           <li className="p-6 border-b-[0.5px] border-white border-opacity-50">
             <Text size="xl" weight="light" className="text-white text-center tracking-[.10em]">
-              No upcoming events for now. Stay tuned!
+              {t("home:upcoming-event-section.notFound")}
             </Text>
           </li>
         ) : (
-          events.map((event) => {
+          events.slice(0, MAX_EVENTS_DISPLAYED).map((event) => {
             return (
               <li key={event._id} className="p-6 border-b-[0.5px] border-white border-opacity-50">
                 <UpcomingEvent
                   name={event.name}
                   description={event.location.label}
                   date={event.startDate}
-                  onClick={() => console.log("on redirect to ticket platform")}
+                  ticketUrl={event.ticket}
                 />
               </li>
             );
@@ -43,11 +50,12 @@ type UpcomingEventProps = {
   name: string;
   description: string;
   date: Date;
-  onClick: () => void;
+  ticketUrl: string;
 };
 
-const UpcomingEvent = ({ name, description, date, onClick }: UpcomingEventProps) => {
+const UpcomingEvent = ({ name, description, date, ticketUrl }: UpcomingEventProps) => {
   const _date = dayjs(date);
+  const { t } = useTranslation(homeNamespaces);
 
   return (
     <div className="flex flex-row items-center">
@@ -63,16 +71,18 @@ const UpcomingEvent = ({ name, description, date, onClick }: UpcomingEventProps)
         <Text size="2xl" className="text-white tracking-[.25em]">
           {name}
         </Text>
-        <Text size="base" className="text-white tracking-[.10em]" weight="light">
+        <Text size="base" className="text-purple tracking-[.10em]" weight="light">
           {description}
         </Text>
       </div>
       <div className="ml-auto">
-        <button onClick={onClick} className="cursor-pointer">
-          <Text size="base" className="text-white tracking-[0.25em]">
-            Tickets
-          </Text>
-        </button>
+        <ExternalLink href={ticketUrl}>
+          <button className="cursor-pointer">
+            <Text size="base" className="text-white tracking-[0.25em]">
+              {t("home:upcoming-event-section.tickets")}
+            </Text>
+          </button>
+        </ExternalLink>
       </div>
     </div>
   );
