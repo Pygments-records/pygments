3b9bedc66f980328a9953521a764a86267b3816b
Ruben de Andrade <ruben.deandrade@fabernovel.com>
2024-02-17 18:46:52 +0100

    feat: design correction / add i18n
    Signed-off-by: Ruben de Andrade <ruben.deandrade@fabernovel.com>

--- webapp/src/core/common/layouts/RootLayout/Footer.tsx
+++ webapp/src/core/common/layouts/RootLayout/Footer.tsx
@@ -1,44 +1,113 @@
 import { ExternalLink } from '@ui-kit/components/external-link/ExternalLink'
 import { Text } from '@ui-kit/components/text/Text'
-import {PygmentsIcon} from "@ui-kit/icons/PygmentsIcon";
+import { PygmentsIcon } from '@ui-kit/icons/PygmentsIcon'
+import styles from './Footer.module.css'
+import { Link } from '@ui-kit/components/link/Link'
+import { useTranslation } from '@core/i18n/useTranslation'
+import NextLink from 'next/link'
 
 export const Footer = () => {
+  const { t } = useTranslation('common')
   return (
-    <footer className="font-pygments" style={{ background: '#408BB5', color: '#2E2E38' }}>
-      <div className="container mx-auto px-8 py-4 flex justify-between text-white">
+    <footer className={styles['footer']}>
+      <section className={styles['navigation']}>
         <div>
-          <Text className="font-bold">Network</Text>
-          <ExternalLink href="https://www.instagram.com/pygments.records/"><Text>Instagram</Text></ExternalLink>
-          <ExternalLink href="https://www.youtube.com/@pygmentsrecords"><Text>You-Tube</Text></ExternalLink>
-          <ExternalLink href="https://www.facebook.com/profile.php?id=100076739332122"><Text>Facebook</Text></ExternalLink>
-          <ExternalLink href="https://soundcloud.com/pygments-records"><Text>SoundCloud</Text></ExternalLink>
-          <ExternalLink href="https://www.tiktok.com/@pygments"><Text>TikTok</Text></ExternalLink>
+          <Text size="lg" weight="bold">
+            {t('footer.nav.network.label')}
+          </Text>
+          <ul className={styles['navigation-list']}>
+            <li>
+              <ExternalLink href="https://www.instagram.com/pygments.records/">
+                <Text>{t('footer.nav.network.instagram')}</Text>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://www.youtube.com/@pygmentsrecords">
+                <Text>{t('footer.nav.network.youtube')}</Text>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://www.facebook.com/profile.php?id=100076739332122">
+                <Text>{t('footer.nav.network.facebook')}</Text>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://soundcloud.com/pygments-records">
+                <Text>{t('footer.nav.network.soundcloud')}</Text>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://www.tiktok.com/@pygments">
+                <Text>{t('footer.nav.network.tiktok')}</Text>
+              </ExternalLink>
+            </li>
+          </ul>
         </div>
         <div>
-          <Text className="font-bold">PartyCrew</Text>
-          <ExternalLink href="https://shotgun.live/fr/venues/pygments-records"><Text>Shotgun</Text></ExternalLink>
-          <ExternalLink href="https://fr.ra.co/promoters/121656"><Text>RA</Text></ExternalLink>
-          <ExternalLink href="http://pygments.io/timeline"><Text>Timeline</Text></ExternalLink>
+          <Text size="lg" weight="bold">
+            {t('footer.nav.party-crew.label')}
+          </Text>
+          <ul className={styles['navigation-list']}>
+            <li>
+              <ExternalLink href="https://shotgun.live/fr/venues/pygments-records">
+                <Text>{t('footer.nav.party-crew.shotgun')}</Text>
+              </ExternalLink>
+            </li>
+            <li>
+              <ExternalLink href="https://fr.ra.co/promoters/121656">
+                <Text>{t('footer.nav.party-crew.ra')}</Text>
+              </ExternalLink>
+            </li>
+            <li>
+              <NextLink passHref={true} href="/timeline">
+                <Link>
+                  <Text>{t('footer.nav.party-crew.timeline')}</Text>
+                </Link>
+              </NextLink>
+            </li>
+          </ul>
         </div>
         <div>
-          <Text className="font-bold">Artists</Text>
-          <ExternalLink href="#"><Text>Residents</Text></ExternalLink>
-          <ExternalLink href="#"><Text>Agency</Text></ExternalLink>
+          <Text size="lg" weight="bold">
+            {t('footer.nav.artists.label')}
+          </Text>
+          <ul className={styles['navigation-list']}>
+            <li>
+              <Link href="javascript:void(0)" className={styles['disabled']} aria-disabled="true">
+                <Text>{t('footer.nav.artists.residents')}</Text>
+              </Link>
+            </li>
+            <li>
+              <Link href="javascript:void(0)" className={styles['disabled']} aria-disabled="true">
+                <Text>{t('footer.nav.artists.agency')}</Text>
+              </Link>
+            </li>
+          </ul>
         </div>
         <div>
-          <Text className="font-bold">(Lab)el</Text>
-          <Text>Coming soon</Text>
+          <Text size="lg" weight="bold">
+            {t('footer.nav.label.label')}
+          </Text>
+          <ul className={styles['navigation-list']}>
+            <li>
+              <Text className={styles['disabled']}>{t('footer.coming-soon')}</Text>
+            </li>
+          </ul>
         </div>
-        <div>
+        <div className="lg:ml-auto">
           <ExternalLink href="mailto:pygments.records@gmail.com">
-            <Text className="text-darkPurple hover:underline  text-white">Contact us !</Text>
+            <Text size="lg" weight="bold" className="text-darkPurple hover:underline text-white">
+              {t('footer.contact-us')}
+            </Text>
           </ExternalLink>
         </div>
-      </div>
-      <PygmentsIcon className="w-[150px] lg:w-[200px] cursor-pointer justify-center" />
-      <div className="text-center text-white">
-        <Text className="text-sm">© 2024 All rights reserved to Pygments Records. Website by @Ruben de Andrade</Text>
-      </div>
+      </section>
+      <section className="flex flex-col items-center mt-4">
+        <PygmentsIcon className="w-[150px] lg:w-[200px]" />
+        <div className="text-white">
+          <Text size="sm">{t('footer.copyright')}</Text>
+        </div>
+      </section>
     </footer>
   )
 }
